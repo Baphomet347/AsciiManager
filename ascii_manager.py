f53b598ed12907a8335f5b7e909f68412f7341a4
Thomas <Thomas-Schillinger@outlook.de>
2020-07-21 13:03:45 +0200

    first commit

---
+++ ascii_manager.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+#pip install pyperclip, 
+#pip install Pillow
+#python3 -m pip install requests
+
+import pyperclip
+import sys
+import getopt
+import os
+
+command = sys.argv[1].lower()
+try: 
+#print 'Argument List:', str(sys.argv)
+#print ("First argument: %s" % str(sys.argv[1]))
+	if command == "-l" or command == "-list":
+		#currentDirectory = pathlib.Path('./txt')
+		#for currentFiles in currentDirectory.iterdir():
+		#removetable = str.maketrans('.txt', 'txt/')
+		#print currentFiles
+		lst = sorted(os.listdir("./txt"))
+		print lst
+	elif command == "-help" or command == "-h":
+		file = open("readme.txt" ,mode='r')
+		datatext= file.read()
+		file.close()
+		print datatext
+	else:
+		textfile = str(command)
+		file = open("txt/"+textfile ,mode='r')
+		datatext= file.read()
+		data = datatext.decode('utf-8')
+		file.close()
+		print "* text saved to clipboard. *"
+		pyperclip.copy(data)
+		
+except Exception as e:
+	print "command or text not found. try ascii -h/-help or ascii -l/-list."
---
+++ readme.txt
@@ -0,0 +1,3 @@
+Usage:
+ascii [emotename] --> copy ascii emote to your clipboard
+ascii -l/-list --> list all available ascii emotes
